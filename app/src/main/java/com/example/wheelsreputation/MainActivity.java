6548c431101268183c65f242a685c0c1edf46b72
JEDIDIA <jedidiamatondo@gmail.com>
2022-11-13 00:00:18 +0200

    included the code to share my app via bleutooth

--- app/src/main/java/com/example/wheelsreputation/MainActivity.java
+++ app/src/main/java/com/example/wheelsreputation/MainActivity.java
@@ -18,6 +18,32 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Drivers Reputation");
     }
 
+    public void shareApkFile () {
+
+        //how to send apk file to another device and recreate the app
+
+        // Get current ApplicationInfo to find .apk path
+     /*   ApplicationInfo app = getApplicationContext().getApplicationInfo();
+        String filePath = app.sourceDir;
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+// MIME of .apk is "application/vnd.android.package-archive".
+// but Bluetooth does not accept this. Let's use "*1/*" instead.
+        intent.setType("*1/*");
+
+// Only use Bluetooth to send .apk
+        intent.setPackage("com.android.bluetooth");
+
+// Append file and send Intent
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(filePath)));
+        startActivity(Intent.createChooser(intent, "Share app"));
+
+
+
+      */
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
