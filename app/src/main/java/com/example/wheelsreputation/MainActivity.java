3941f290441d4e4b51d69750d7e90fc4d053bc52
JEDIDIA <jedidiamatondo@gmail.com>
2022-11-12 14:57:44 +0200

    upgraded the ui and set a menu

--- app/src/main/java/com/example/wheelsreputation/MainActivity.java
+++ app/src/main/java/com/example/wheelsreputation/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.wheelsreputation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("Drivers Reputation");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_first_page,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+            case R.id.username:
+                // define the username of the user and set text to his name
+
+                return true;
+
+            case R.id.add_something:
+                //send to an activity with a compsed window to choose between the 3 toggle options
+
+                return true;
+
+            case R.id.logout:
+                //logs the user out
+
+                return true;
+
+            default:
+                return false;
+        }
+
     }
 }
