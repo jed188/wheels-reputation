7f39d1292a66cd8054962c5746f791198c25e383
JEDIDIA <jedidiamatondo@gmail.com>
2022-11-30 10:26:45 +0200

    additionto files to git with add and commit

---
+++ app/src/main/java/com/example/wheelsreputation/Find_driver_activity.java
@@ -0,0 +1,17 @@
+package com.example.wheelsreputation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Find_driver_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_driver);
+
+        setTitle("Drivers Reputation");
+
+    }
+}
