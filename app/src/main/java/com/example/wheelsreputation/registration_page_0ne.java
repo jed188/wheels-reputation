28a94344c2cd5e0dd0f735a6472f5a0a01bb6832
JEDIDIA <jedidiamatondo@gmail.com>
2022-12-27 16:01:58 +0200

    registration window done. constraints to be added.

---
+++ app/src/main/java/com/example/wheelsreputation/registration_page_0ne.java
@@ -0,0 +1,14 @@
+package com.example.wheelsreputation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration_page_0ne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page0ne);
+    }
+}
