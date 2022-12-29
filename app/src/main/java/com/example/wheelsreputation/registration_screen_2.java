cf6bedea69dcbd1821695aa558c4faa905a11a40
JEDIDIA <jedidiamatondo@gmail.com>
2022-12-29 14:01:37 +0200

    second registration window added due to constraints of size add something window to be added

---
+++ app/src/main/java/com/example/wheelsreputation/registration_screen_2.java
@@ -0,0 +1,14 @@
+package com.example.wheelsreputation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registration_screen_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_screen2);
+    }
+}
