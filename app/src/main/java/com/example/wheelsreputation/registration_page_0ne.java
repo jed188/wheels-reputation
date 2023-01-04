f72ca768d2b8844d23193efc57d9c475dd5fe22b
JEDIDIA <jedidiamatondo@gmail.com>
2023-01-04 07:59:10 +0200

    registration pages completed login page to be connected

--- app/src/main/java/com/example/wheelsreputation/registration_page_0ne.java
+++ app/src/main/java/com/example/wheelsreputation/registration_page_0ne.java
@@ -2,13 +2,30 @@ package com.example.wheelsreputation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class registration_page_0ne extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page0ne);
+
+        button = findViewById(R.id.button3);
+
     }
+
+    public void next_activity (View view){
+
+        Intent intent = new Intent(this,registration_screen_2.class);
+
+        startActivity(intent);
+
+    }
+
 }
--- app/src/main/java/com/example/wheelsreputation/registration_screen_2.java
+++ app/src/main/java/com/example/wheelsreputation/registration_screen_2.java
@@ -1,14 +1,104 @@
 package com.example.wheelsreputation;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class registration_screen_2 extends AppCompatActivity {
 
+    Button browseFiles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen2);
+
+        browseFiles = findViewById(R.id.button6);
+
+
+
     }
+
+    public void browse_files(View view){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+
+            } else {
+
+                getPhoto();
+
+            }
+        }
+
+    }
+
+    private void getPhoto() {
+
+        Intent intent = new Intent (Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        activityLauncher.launch(intent);
+
+    }
+
+    private ActivityResultLauncher<Intent> activityLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult() , new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+
+            if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null){
+
+                Uri selectedImage = result.getData().getData();
+
+                try{
+
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(),selectedImage);
+
+                    ImageView imageView = findViewById(R.id.imageView3);
+
+                    imageView.setImageBitmap(bitmap);
+
+                }
+                catch(Exception e){
+
+                    e.printStackTrace();
+
+                }
+
+            }
+        }
+    });
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode , @NonNull String[] permissions , @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode , permissions , grantResults);
+
+        if(requestCode == 1){
+
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                getPhoto();
+
+            }
+
+        }
+
+    }
+
 }
