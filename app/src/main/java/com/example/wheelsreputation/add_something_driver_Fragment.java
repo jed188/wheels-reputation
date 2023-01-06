64e17767ac000961c969071a8b3d04574bfec526
JEDIDIA <jedidiamatondo@gmail.com>
2023-01-06 21:58:02 +0200

    driver fragment completed minus the save button review fragment in progress of completion

--- app/src/main/java/com/example/wheelsreputation/add_something_driver_Fragment.java
+++ app/src/main/java/com/example/wheelsreputation/add_something_driver_Fragment.java
@@ -4,20 +4,27 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.window.SplashScreen;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 
 public class add_something_driver_Fragment extends Fragment {
 
 
     String[] choice = {"Yes","No"};
-    AutoCompleteTextView autocomplete;
+    Spinner spinner;
+    Button save;
 
     @Override
     public View onCreateView(LayoutInflater inflater , ViewGroup container ,
@@ -25,11 +32,22 @@ public class add_something_driver_Fragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_something_driver_ , container , false);
 
-        autocomplete = view.findViewById(R.id.autoCompleteTextView);
+        spinner = view.findViewById(R.id.spinner);
+        ArrayAdapter adapter =  new ArrayAdapter(getContext(),R.layout.dropdown,choice);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter(getContext(),R.layout.dropdown,choice);
+        spinner.setAdapter(adapter);
 
-        autocomplete.setAdapter(adapter);
+
+        save = view.findViewById(R.id.button_add_driver);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i ("itemVelue",spinner.getSelectedItem().toString());
+            }
+        });
+
 
         return view;
     }
