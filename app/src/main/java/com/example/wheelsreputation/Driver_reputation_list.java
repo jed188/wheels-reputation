3214e46cb6a39f83aafce67863a9e03a950ca430
JEDIDIA <jedidiamatondo@gmail.com>
2023-01-10 13:12:55 +0200

    most pages are done and the textview details are to be updated the fragment tabs are to be perfected for color difference, style and text

--- app/src/main/java/com/example/wheelsreputation/Driver_reputation_list.java
+++ app/src/main/java/com/example/wheelsreputation/Driver_reputation_list.java
@@ -1,10 +1,16 @@
 package com.example.wheelsreputation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import java.util.List;
 
@@ -17,6 +23,8 @@ public class Driver_reputation_list extends AppCompatActivity {
     RecyclerView.Adapter mAdapterReputation;
 
     RecyclerView.LayoutManager layoutManagerReputation;
+
+    Intent intent;
 
     //global_variable globalVariable = (global_variable) this.getApplication();
 
@@ -41,5 +49,61 @@ public class Driver_reputation_list extends AppCompatActivity {
 
         recyclerReputation.setAdapter(mAdapterReputation);
 
+    }
+
+    public void stolenBikeMethod(View view){
+
+        intent = new Intent(this , Stolen_bike_list.class);
+
+        startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_first_page,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+            case R.id.username:
+                // define the username of the user and set text to his name
+
+                intent = new Intent(this,login_Activity.class);
+
+                startActivity(intent);
+
+                return true;
+
+            case R.id.add_something:
+                //send to an activity with a compsed window to choose between the 3 toggle options
+
+                intent = new Intent(this,Add_something.class);
+
+                startActivity(intent);
+
+                return true;
+
+            case R.id.search:
+
+                intent = new Intent(this,MainActivity.class);
+
+                startActivity(intent);
+
+            case R.id.logout:
+                //logs the user out
+
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
